19a04e209f25a252f7cdfba524895e64a5f34c37
sangam919 <sangamsri555@gmail.com>
2025-12-26 18:09:39 +0530

    Added DP Practice File

---
+++ DP/fibonacci.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int f(int n,vector<int>& dp){
+
+    if(n<=1) return n;
+
+    if(dp[n]!=-1) return dp[n];
+
+    dp[n]=f(n-1,dp)+f(n-2,dp);
+
+    return dp[n];
+}
+int main(){
+    int n;
+    cin >> n;
+    vector<int> dp(n+1,-1);
+    cout<<f(n,dp);
+    return 0;
+
+}
